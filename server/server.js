3cd107c0d6501c9744b1548106b9856e214526b3
Jon Oh <jonathano@zillowgroup.com>
2021-07-24 21:17:41 -0700

    Add heroku commands

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV !== "production") {
 const cors = require('cors')
 const express = require('express');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(express.json());
